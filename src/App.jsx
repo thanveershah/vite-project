9db552d65e88d650c707c75c3199f3541480676d
thanveershah <thanveershah92@gmail.com>
2022-10-18 02:09:21 +0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "@/components/header";
+
+function App() {
+   return (
+      <div>
+         <h1>Test Component Import</h1>
+         <Header />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+   return <header>It doesn't work</header>;
+};
